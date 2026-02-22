12adee8f9f01bb85c86461a837283fac81bff80c
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2026-02-22 21:41:41 +1000

    add 2026
    add 2026

--- kevinmenu.py
+++ kevinmenu.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.write("# Welcome to Kevin 의  챗봇 ! 👋")
+st.write("### Welcome to Kevin 의  챗봇 ! 👋")
 
 with st.sidebar:
      
      st.markdown(
       """
-          아래 를 선택 하시고 음미 해보세요
-      
+          아래 를 선택 하시고 음미 해보세요.
+                
           - [2023년 X-Mas Card ](https://youtu.be/-i3qjuYwkkI)
           - [결혼 43주년 ](https://youtu.be/z_BM9oS_tVQ)
           - [2023 Youth REM ](https://youtu.be/2l8QLk4pE_4)
@@ -24,6 +24,7 @@ with st.sidebar:
           - [2025 NZ Cruise 여행기 ](https://kevinoh-nzcruise.streamlit.app/)
           - [끌어 당김에 법칙 ](https://kevinoh-pull00.streamlit.app/)
           - [회계하라 ](https://kevinoh-repent.streamlit.app/)
+          - [트라우마 극복하기 ](https://kevinoh-trauma2.streamlit.app/)
          
 
     """
@@ -32,16 +33,20 @@ with st.sidebar:
 
 st.markdown(
     """
-    살다 보면 즐거운 일도 있지만 말못할 고민이 있게 됩니다.
-    사람한태 물어 보가 힘들때   
-    여기 Kevin 의 심리 상담 챗봇 한테 문의를 ..
+    아래 주제별 챗봇을 질문하고 음미 해보시길
+         클릭하고 "<<"  나오면 클릭하고 선택해서 분야별 챗봇을 선탹 
+      종교(성경해설) 
+      심리학(고민상담) 
+      의학 (질병) 
+      영어 (회화, 해설) 기타
 
 
-    - [심리 상담 챗봇 ](https://2024-app4-8yc8qc8xa6jacxc3mjtyw4.streamlit.app/)
+    - [주제별 챗봇 ](https://helenoh-chatbot1.streamlit.app/)
     
     
     업무를 하다보면 문서를 번역 한다던지 요약한다던지 내용의 의미를 묻고 싶을때 
-    Kevin 의 챗봇을 통해 물어 보세요.  먼저 볼 File 를 upload 하시고   
+    Kevin 의 챗봇을 통해 물어 보세요.  
+    아래를 클릭하고 먼저 볼 File 를 upload 하시고   
     
     - [Kevin 의 챗봇 ](https://langchain6-gwtklymlqjbtytbcaestpf.streamlit.app/)
 
