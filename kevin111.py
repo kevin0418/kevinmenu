ba663281022dbb029a87c748589d5065c5591c7d
kevin0418 <54438552+kevin0418@users.noreply.github.com>
2026-05-29 21:27:05 +1000

    update sub
    update sub

---
+++ kevin111.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.write("# Welcome to Kevin 의  챗봇 ! 👋")
+
+with st.sidebar:
+     
+     st.markdown(
+      """
+          부동산 관련 자료
+      
+          - [부동산 질문 file upload (pdf, doc)](https://kevinoh-realagent20.streamlit.app/)
+          - [부동산 질문 website ](https://kevinoh-realagent30.streamlit.app/)
+          
+          - [용 부동산 자료 ](https://kevinoh-yongmenu.streamlit.app)
+         
+
+                 
+    """
+    )
+     
+
+st.markdown(
+    """
+    살다 보면 즐거운 일도 있지만 말못할 고민이 있게 됩니다.
+    사람한태 물어 보가 힘들때   
+    여기 Kevin 의 심리 상담 챗봇 한테 문의를 ..
+   
+    - [Text to image, 그림 그리기 ](https://kevinoh-draw1.streamlit.app/)
+    - [Image 분석 해설 영어로도 ](https://kevinoh-image1.streamlit.app/)
+    - [바둑 이란  ](https://kevinoh-gogame.streamlit.app/)
+    
+
+    어학 study
+    - [어원 학습기 ](https://kevinoh-engbot3.streamlit.app/)
+    - [질문 answer ](https://helenoh.streamlit.app/)
+    - [다중 회회해설 챗봇 Gemini ](https://helenoh-chatbot1.streamlit.app/)
+    - [단어 어원 학습기 ](https://kevinoh-engbot3.streamlit.app/)
+
+    심리상담, 의학, 종교 등등
+     - [다중 주제 챗봇 openai ](https://helenoh-chatbot.streamlit.app/)
+     - [문제 생성기 ](https://kevinoh-test91.streamlit.app/)
+
+   
+
+    
+"""
+)
---
+++ kevin222.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.write("# Welcome to Kevin 의  유트브 챗봇 ! 👋")
+
+with st.sidebar:
+     
+     st.markdown(
+      """
+          아래 를 선택 하시고 음미 해보세요
+      
+          - [2023년 X-Mas Card ](https://youtu.be/-i3qjuYwkkI)
+          - [결혼 43주년 ](https://youtu.be/z_BM9oS_tVQ)
+          
+          - [2023 Youth REM ](https://youtu.be/2l8QLk4pE_4)
+          - [2025 X-Mas 마니또 성경구절 ](https://kevinoh-bible1.streamlit.app/)
+
+          
+          - [나도 누구인가 ](https://kevinoh-whoami.streamlit.app/)
+          - [나도 부활할수 있나 ](https://youtu.be/HT-7nTMv8Ak)
+          - [용서 ](https://youtu.be/Pt5_qFeGl3Q)
+          - [상상력 의 힘 ](https://youtu.be/4bSIVsGl7GM)
+          - [끌어 당김에 법칙 ](https://kevinoh-pull00.streamlit.app/)
+          - [트라우마 극복하기 ](https://kevinoh-trauma2.streamlit.app/)
+          - [우환 때문에 산다 ](https://www.youtube.com/watch?v=0FV7rqP06c8)
+          
+          - [Body Language 알아채기 ](https://kevinoh-bodylanguage.streamlit.app/)
+          - [NZ 쿠르즈 여행기 ](https://kevinoh-nzcruise.streamlit.app/)
+          - [2026 REM workshop ]( https://kevin-rem209.streamlit.app/)
+         
+          
+                 
+    """
+    )
+     
+
+st.markdown(
+    """
+    살다 보면 즐거운 일도 있지만 말못할 고민이 있게 됩니다.
+    사람한태 물어 보가 힘들때   
+    여기 Kevin 의 심리 상담 챗봇 한테 문의를 ..
+
+
+    - [심리 상담 챗봇 ](https://2024-app4-8yc8qc8xa6jacxc3mjtyw4.streamlit.app/)
+    
+    
+    업무를 하다보면 문서를 번역 한다던지 요약한다던지 내용의 의미를 묻고 싶을때 
+    Kevin 의 챗봇을 통해 물어 보세요.  먼저 볼 File 를 upload 하시고   
+    
+    - [Kevin 의 챗봇 ](https://langchain6-gwtklymlqjbtytbcaestpf.streamlit.app/)
+
+    
+"""
+)
